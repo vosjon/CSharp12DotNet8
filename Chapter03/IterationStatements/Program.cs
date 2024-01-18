2ace9ae2178b7c9f26f0ae01d548e728c84e584e
Jonathan Vos <vosjonjoe@gmail.com>
2024-01-17 18:17:17 -0800

    Completed two more sections of Chapter03.

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,45 @@
+﻿int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+/*string? actualPassword = "Pa$$w0rd";
+string? password;
+int attempts = 6;
+do
+{
+    Write("Enter your password: ");
+    password = ReadLine();
+
+    if (password != actualPassword)
+    {
+        attempts--;
+        WriteLine($"That was incorrent. You have {attempts} remaining.");
+        if (attempts == 0)
+        {
+            break;
+        }   
+    }
+}
+while (password != actualPassword);
+WriteLine("Correct!");
+*/
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
+
+string[] names = { "Adam", "Barry", "Charlie" };
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
---
+++ Chapter03/SelectionStatements/Animals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatements;
+
+class Animal // This is the base type for all animals.
+{
+    public string? Name;
+    public DateTime Born;
+    public byte Legs;
+}
+class Cat : Animal // This is a subtype of animal.
+{
+    public bool IsDomestic;
+}
+class Spider : Animal // This is another subtype of animal.
+{
+    public bool IsPoisonous;
+}
