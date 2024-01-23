7aec20f6d5438ea5f0d576d664af10bd07dcc826
Jonathan Vos <vosjonjoe@gmail.com>
2024-01-22 18:31:23 -0800

    Started the Chapter05 solution.

---
+++ Chapter05/PeopleApp/Program.Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+partial class Program
+{
+    private static void ConfigureConsole(
+        string culture = "en-US",
+        bool useComputerCulture = false,
+        bool showCulture = true)
+    {
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useComputerCulture)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        if (!showCulture)
+        {
+            WriteLine($"Current culture: {CultureInfo.CurrentCulture.DisplayName}");
+        }
+    }
+}
