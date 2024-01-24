9cd85441ffc0cab7828c3295f7baebfadf9a4bb6
Jonathan Vos <vosjonjoe@gmail.com>
2024-01-23 19:01:35 -0800

    Made more progress on Chapter05.

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -4,6 +4,21 @@ using System.Text;
 
 namespace Packt.Shared;
 
-public class Person
+public class Person : object
 {
+    #region Fields: Data or state for this person.
+    public string? Name; // ? means it can be null.
+    public DateTimeOffset Born;
+    public WondersOfTheAncientWorld FavoriteAncientWonder;
+    public WondersOfTheAncientWorld BucketList;
+    #endregion
 }
+
+
+
+
+
+
+
+
+
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+ConfigureConsole();
+
+Person bob = new Person();
+
+bob.Name = "Bob";
+
+WriteLine(bob);
+WriteLine($"Peron's name is: {bob.Name}");
+
+bob.Name = "Bob Smith";
+bob.Born = new DateTimeOffset(
+  year: 1965, month: 12, day: 22,
+  hour: 16, minute: 28, second: 0,
+  offset: TimeSpan.FromHours(-5)); // US Eastern Standard Time.
+WriteLine(format: "{0} was born on {1:D}.", // Long date.
+  arg0: bob.Name, arg1: bob.Born);
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    Born = new(1998, 3, 7, 16, 28, 0,
+    // This is an optional offset from UTC time zone.
+    TimeSpan.Zero)
+};
+WriteLine(format: "{0} was born on {1:d}.", // Short date.
+  arg0: alice.Name, arg1: alice.Born);
+
+
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+WriteLine(
+  format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+  arg0: bob.Name,
+  arg1: bob.FavoriteAncientWonder,
+  arg2: (int)bob.FavoriteAncientWonder);
+
+bob.BucketList =
+  WondersOfTheAncientWorld.HangingGardensOfBabylon
+  | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+// bob.BucketList = (WondersOfTheAncientWorld)18;
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
+
